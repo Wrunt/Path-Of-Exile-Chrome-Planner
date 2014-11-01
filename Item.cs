58b1ee48d989e745fb2aa1acfaba0ab8be16ac4c
Wruntjunior <wrunt@greyonline.net>
2014-11-01 01:25:19 -0400

    Initial Commit
    commit

---
+++ Item.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Chroming_Guide
+{
+    class Item
+    {
+        private readonly int x = 16;
+        public int Sockets { get; set; }
+        public int Dex { get; set; }
+        public int Str { get; set; }
+        public int Int { get; set; }
+        public double MinimumPercentage { get; set; }
+        public double Chance { get; set; }
+
+        public int DesiredBlue { get; set; }
+        public int DesiredGreen { get; set; }
+        public int DesiredRed { get; set; }
+
+        public double BlueProbability { get { return (double)(Int + x) / (Dex + Str + Int + (3 * x)); } }
+        public double GreenProbability { get { return (double)(Dex + x) / (Dex + Str + Int + (3 * x)); } }
+        public double RedProbability { get { return (double)(Str + x) / (Dex + Str + Int + (3 * x)); } }
+
+        public Item(int soc, int dex, int str, int inte, double min = .95)
+        {
+            Sockets = soc;
+            Dex = dex;
+            Str = str;
+            Int = inte;
+            MinimumPercentage = min;
+        }
+
+        public void SetDesiredColors(int g, int b, int r)
+        {
+            DesiredBlue = b;
+            DesiredGreen = g;
+            DesiredRed = r;
+            Chance = ((DesiredBlue * BlueProbability) + (DesiredGreen + GreenProbability) + (DesiredRed + RedProbability)) / Sockets;
+        }
+
+        public int GetNumberOfChroms()
+        {
+            double baseChance = 1 - (Chance / 100);
+            
+            int tries = 1;
+
+            for (double currentPercent = 0; currentPercent < MinimumPercentage; tries++)
+            {
+                currentPercent = 1 - Math.Pow(baseChance, tries);
+            }
+
+            return tries;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Chroming_Guide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of sockets on item: ");
+            int sockets = Int32.Parse(Console.ReadLine());
+
+            Console.Write("\r\n\r\nInt on the item: ");
+            int i = Int32.Parse(Console.ReadLine());
+
+            Console.Write("\r\nStr on the item: ");
+            int s = Int32.Parse(Console.ReadLine());
+
+            Console.Write("\r\nDex on the item: ");
+            int d = Int32.Parse(Console.ReadLine());
+
+            Item item = new Item(sockets, d, s, i);
+
+            Console.Clear();
+
+            Console.Write("Enter desired number of blue sockets: ");
+            int b = Int32.Parse(Console.ReadLine());
+            
+            Console.Write("\r\nEnter desired number of green sockets: ");
+            int g = Int32.Parse(Console.ReadLine());
+            
+            Console.Write("\r\nEnter desired number of red sockets: ");
+            int r = Int32.Parse(Console.ReadLine());
+
+            item.SetDesiredColors(g, b, r);
+
+            Console.Clear();
+
+            Console.WriteLine(string.Format("Chance of getting {0} blue, {1} green, and {2} red sockets: {3}.", item.DesiredBlue, item.DesiredGreen, item.DesiredRed, item.Chance));
+            Console.WriteLine("\r\nExpected number of chromatics for 95% success: " + item.GetNumberOfChroms());
+            Console.Write("\r\n\r\n\r\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
